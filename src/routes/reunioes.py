c8984a7ca55c5d92762a5129e88a129391b4d80f
Leocyberdev <151261062+Leocyberdev@users.noreply.github.com>
2025-07-17 11:04:41 -0300

    Update reunioes.py

--- src/routes/reunioes.py
+++ src/routes/reunioes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, session
 from src.models.user import User, Reuniao, db
 from src.routes.auth import login_required
-from datetime import datetime
+from datetime import datetime, date, time
 from src.email_service import email_service
 
 
